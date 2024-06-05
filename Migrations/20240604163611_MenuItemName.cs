2c8cca1516e5e8a6fe1218b5dfc77006dcbc8f2f
Diane Ronda Tampon <d61537072@gmail.com>
2024-06-05 23:22:41 +0800

    migration

---
+++ Migrations/20240604163611_MenuItemName.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantsWebApi.Migrations
+{
+    public partial class MenuItemName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NameOfThePizza",
+                table: "MenuItem",
+                newName: "Name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Restaurant",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Restaurant",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "MenuItem",
+                newName: "NameOfThePizza");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Restaurant",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Restaurant",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
